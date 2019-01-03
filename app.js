6b27aad3589a0af55a137801a025669a304e225e
nullmicgo <nullmicgo@nullmicgos-MacBook-Pro.local>
2019-01-03 19:00:12 +0800

    Adding Multiple Routes to the App
    Adding Multiple Routes to the App
    
    Co-Authored-By: Nullmicgo <nullmcigo@gmail.com>

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 
 
-app.get('/',(request, response) =>{
-    response.send('I LOVE HSBC');
+app.get('/',(req, res) =>{
+    res.send('<h1>I LOVE HSBC</h1>');
 });
 
+app.get('/hello',(req, res) =>{
+    res.send('<h1>Hello Javascript HAHA</h1>');
+});
 
-app.listen(3000);
+app.listen(3000, () =>{
+    console.log("The application is running on localhost:3000!");
+});
