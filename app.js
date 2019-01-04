90ae5125b0a35d4bd28b7c32b32647be7b045718
nullmicgo <nullmicgo@nullmicgos-MacBook-Pro.local>
2019-01-04 17:17:04 +0800

    Statelessness, Setting and Reading Cookies

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
-
+app.use(cookieParser());
 
 const colors = [
     'red',
@@ -24,16 +26,21 @@ app.get('/',(req, res) =>{
 });
 
 
+
 app.get('/hello',(req,res) => {
-    res.render('hello');
-});
+    //res.json(req.body);
+     res.render('hello',{name: req.cookies.username});
+ 
+ });
+
+ 
 
 
 app.post('/hello',(req,res) => {
-   //res.json(req.body);
-    res.render('hello',{name: req.body.username});
-
+    res.cookie('username',req.body.username);
+    res.render('hello',{name:req.body.username});
 });
+
 
 
 
