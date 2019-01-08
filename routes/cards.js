f3ed21543186df88b63e7e36284ab82b3fee4c1d
nullmicgo <nullmicgo@nullmicgos-MacBook-Pro.local>
2019-01-08 14:59:19 +0800

    Improve Card Template

--- routes/cards.js
+++ routes/cards.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards }  = data;
 
-router.get('/:id',(req, res) =>{  
-    res.render('card',{ 
-        prompt: cards[req.params.id].question,
-        hint:cards[req.params.id].hint
-    });
+router.get('/:id',(req, res) =>{
+    const {side} = req.query;
+    const {id } = req.params;
+    const text =cards[id][side];
+    const {hint} =cards[id];
+    const templateData = {text, hint};
+
+    res.render('card',templateData);
  });
  
  module.exports = router;
