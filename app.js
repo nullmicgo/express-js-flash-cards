6719933b9a47e16a0651afc782f7b8c8871bcc3c
nullmicgo <nullmicgo@nullmicgos-MacBook-Pro.local>
2019-01-08 16:26:09 +0800

    Applying styling into app

--- app.js
+++ app.js
@@ -4,25 +4,11 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
-app.use('/static',express.static('public'));
-app.use((req,res,next) =>{
-    console.log('world');
-    next(); 
-});
+app.use('/static', express.static('public'));
 
-const colors = [
-    'red',
-    'oragne',
-    'yellow',
-    'green',
-    'blue',
-    'purple'
-]
-
-//define
-app.set('view engine','pug');
+app.set('view engine', 'pug');
 
 const mainRoutes = require('./routes');
 const cardRoutes = require('./routes/cards');
@@ -30,19 +16,18 @@ const cardRoutes = require('./routes/cards');
 app.use(mainRoutes);
 app.use('/cards', cardRoutes);
 
-
-app.use((req,res,next) =>{
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.use((err, req, res, next) =>{
-    res.locals.error = err;
-    res.status(err.status);
-    res.render('error');
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
 });
 
-app.listen(3000, () =>{
-    console.log("The application is running on localhost:3000!");
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
 });
